efccf10e184b1293e8173f3c3e9a3acc0ced7e0e
Andrew <andrew.potapenko17@gmail.com>
2024-12-20 23:04:21 +0200

    first commit

---
+++ lessonixapp/static/js/firebase-config.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    "apiKey": "AIzaSyBZzUp6wSEKtrhxvvAWUbmMCKw6ST5wrtg",
+    "authDomain": "lessonix-0509.firebaseapp.com",
+    "databaseURL": "https://lessonix-0509-default-rtdb.europe-west1.firebasedatabase.app",
+    "projectId": "lessonix-0509",
+    "storageBucket": "lessonix-0509.appspot.com",
+    "messagingSenderId": "271642966088",
+    "appId": "1:271642966088:web:d5403b9d63c27a93a7c07d",
+};
+
+firebase.initializeApp(firebaseConfig);
